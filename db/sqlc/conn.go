c6acec429f54e70daf501b6a291708d43eaa269f
RageNeko26 <rneko2006@gmail.com>
2023-07-08 00:24:21 +0700

    feat: add inital connection to database

---
+++ db/sqlc/conn.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+var conn *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	conn, err = sql.Open("postgres", "postgres://root:root@localhost:5432/post_app?sslmode=disable")
+
+	if err != nil {
+		panic("Failed to connect Database!")
+	}
+
+	New(conn)
+	os.Exit(m.Run())
+}
