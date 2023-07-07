b52d0b9e35d7788cdfb5005dc6d5e91393be827a
RageNeko26 <rneko2006@gmail.com>
2023-07-08 02:43:22 +0700

    sqlc: Add query for mail records

---
+++ db/query/mail.sql
@@ -0,0 +1,22 @@
+-- name: CreateMail :one
+INSERT INTO mails (
+ title, body, receiver, sender 
+) VALUES (
+  $1, $2, $3, $4
+) RETURNING *;
+
+-- name: ListMails :many
+SELECT * FROM mails
+WHERE receiver = $1;
+
+-- name: GetMail :one
+SELECT * FROM mails
+WHERE id = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE mails SET title = $2, body = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM mails WHERE id = $1;
