8d377a75bb1781c9d2c824d2f3a090f2fda69211
RageNeko26 <rneko2006@gmail.com>
2023-07-08 09:43:40 +0700

    feat: add test case for Create new records

---
+++ db/sqlc/mail_test.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateMail(t *testing.T) {
+	newMail, err := q.CreateMail(context.Background(), CreateMailParams{
+		Title:    "Greet New Neighbour",
+		Body:     "Hi, my name Steven. I live next to your house!",
+		Receiver: 2,
+		Sender:   3,
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, newMail)
+}
